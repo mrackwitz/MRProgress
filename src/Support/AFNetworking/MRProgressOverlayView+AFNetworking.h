4406dfa48ef31b771eb47738ded4a6115384798d
Marius Rackwitz <git@mariusrackwitz.de>
2014-05-31 17:22:28 +0200

    Added boilerplate category files to project

---
+++ src/Support/AFNetworking/MRProgressOverlayView+AFNetworking.h
@@ -0,0 +1,15 @@
+//
+//  MRProgressOverlayView+AFNetworking.h
+//  MRProgress
+//
+//  Created by Marius Rackwitz on 12.03.14.
+//  Copyright (c) 2014 Marius Rackwitz. All rights reserved.
+//
+
+#import <MRProgress/MRProgress.h>
+#import <AFNetworking/AFNetworking.h>
+
+
+@interface MRProgressOverlayView (AFNetworking)
+
+@end
---
+++ src/Support/AFNetworking/MRProgressOverlayView+AFNetworking.m
@@ -0,0 +1,15 @@
+//
+//  MRProgressOverlayView+AFNetworking.m
+//  MRProgress
+//
+//  Created by Marius Rackwitz on 12.03.14.
+//  Copyright (c) 2014 Marius Rackwitz. All rights reserved.
+//
+
+#import "MRProgressOverlayView+AFNetworking.h"
+#import <AFNetworking/AFNetworking.h>
+
+
+@implementation MRProgressOverlayView (AFNetworking)
+
+@end
