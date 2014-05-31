8dd87f5f50c9f5b61bb11690585ec1ebd89d8fbc
Marius Rackwitz <git@mariusrackwitz.de>
2014-06-01 00:36:09 +0200

    Added MRProgressView as intermediate class

---
+++ src/Components/MRProgressView.m
@@ -0,0 +1,14 @@
+//
+//  MRProgressView.m
+//  MRProgress
+//
+//  Created by Marius Rackwitz on 31.05.14.
+//  Copyright (c) 2014 Marius Rackwitz. All rights reserved.
+//
+
+#import "MRProgressView.h"
+
+
+@implementation MRProgressView
+
+@end
