2e59f6d11d7f433aee48f55e1989b54c0922a48b
Marius Rackwitz <git@mariusrackwitz.de>
2014-07-16 14:53:22 +0200

    Added MRAFNetworkingSupportViewController

---
+++ Example/MRAFNetworkingSupportViewController.h
@@ -0,0 +1,14 @@
+//
+//  MRAFNetworkingSupportViewController.h
+//  Example
+//
+//  Created by Marius Rackwitz on 16.07.14.
+//  Copyright (c) 2014 Marius Rackwitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MRAFNetworkingSupportViewController : UIViewController
+
+@end
---
+++ Example/MRAFNetworkingSupportViewController.m
@@ -0,0 +1,28 @@
+//
+//  MRAFNetworkingSupportViewController.m
+//  Example
+//
+//  Created by Marius Rackwitz on 16.07.14.
+//  Copyright (c) 2014 Marius Rackwitz. All rights reserved.
+//
+
+#import "MRAFNetworkingSupportViewController.h"
+#import "AFNetworking.h"
+
+
+@interface MRAFNetworkingSupportViewController ()
+
+
+
+@end
+
+
+@implementation MRAFNetworkingSupportViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+@end
