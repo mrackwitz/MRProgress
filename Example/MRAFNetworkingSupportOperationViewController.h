b903e6e8236e8c6e3584de84075eaee37b434bfc
Marius Rackwitz <git@mariusrackwitz.de>
2014-07-18 16:54:34 +0200

    Add an example for AFNetworking's NSURLConnection-based operation support

---
+++ Example/MRAFNetworkingSupportOperationViewController.h
@@ -0,0 +1,13 @@
+//
+//  MRAFNetworkingSupportOperationViewController.h
+//  Example
+//
+//  Created by Marius Rackwitz on 18.07.14.
+//  Copyright (c) 2014 Marius Rackwitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRAFNetworkingSupportOperationViewController : UIViewController
+
+@end
