7d87403d44c5d7c70f641254fc0aab06332b130e
Marius Rackwitz <git@mariusrackwitz.de>
2014-07-18 12:54:19 +0200

    Unified documentation style

--- src/Utils/MRMethodCopier.h
+++ src/Utils/MRMethodCopier.h
@@ -15,19 +15,19 @@
 @property (nonatomic, assign) Class targetClass;
 
 /**
- *  Instantiate a new method copier
- *
- *  @param originClass origin class, where implementation will be linked to
- *  @param targetClass target class, where the new methods will be added
- *
- *  @return MRMethodCopier
+ Instantiate a new method copier
+ 
+ @param originClass origin class, where implementation will be linked to
+ @param targetClass target class, where the new methods will be added
+ 
+ @return MRMethodCopier
  */
 + (instancetype)copierFromClass:(Class)originClass toClass:(Class)targetClass;
 
 /**
- *  Copy an instance method from the receivers originClass to targetClass
- *
- *  @param selector the given selector where the method is found and will be found
+ Copy an instance method from the receivers originClass to targetClass
+ 
+ @param selector the given selector where the method is found and will be found
  */
 - (void)copyInstanceMethod:(SEL)selector;
 
