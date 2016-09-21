d63ec5e5949bbb7147f91e5b11bd68ac56d1d984
Ryoga Kitagawa <ryoga.kitagawa@gmail.com>
2016-09-21 13:20:03 +0900

    Add new header.

---
+++ src/MRProgress_AFNetworking.h
@@ -0,0 +1,51 @@
+//
+//  MRProgress_AFNetworking.h
+//  MRProgress
+//
+//  Created by Ryoga Kitagawa.
+//
+
+#if __has_include(<MRProgress_AFNetworking/MRActivityIndicatorView.h>)
+#import <MRProgress_AFNetworking/MRActivityIndicatorView.h>
+#endif
+
+#if __has_include(<MRProgress_AFNetworking/MRBlurView.h>)
+#import <MRProgress_AFNetworking/MRBlurView.h>
+#import <MRProgress_AFNetworking/UIImage+MRImageEffects.h>
+#endif
+
+#if __has_include(<MRProgress_AFNetworking/MRCircularProgressView.h>)
+#import <MRProgress_AFNetworking/MRCircularProgressView.h>
+#endif
+
+#if __has_include(<MRProgress_AFNetworking/MRIconView.h>)
+#import <MRProgress_AFNetworking/MRIconView.h>
+#endif
+
+#if __has_include(<MRProgress_AFNetworking/MRMessageInterceptor.h>)
+#import <MRProgress_AFNetworking/MRMessageInterceptor.h>
+#endif
+
+#if __has_include(<MRProgress_AFNetworking/MRNavigationBarProgressView.h>)
+#import <MRProgress_AFNetworking/MRNavigationBarProgressView.h>
+#endif
+
+#if __has_include(<MRProgress_AFNetworking/MRProgressOverlayView.h>)
+#import <MRProgress_AFNetworking/MRProgressOverlayView.h>
+#endif
+
+#if __has_include(<MRProgress_AFNetworking/MRWeakProxy.h>)
+#import <MRProgress_AFNetworking/MRWeakProxy.h>
+#endif
+
+#if __has_include(<MRProgress_AFNetworking/MRActivityIndicatorView+AFNetworking.h>)
+#import <MRProgress_AFNetworking/MRActivityIndicatorView+AFNetworking.h>
+#endif
+
+#if __has_include(<MRProgress_AFNetworking/MRProgressView+AFNetworking.h>)
+#import <MRProgress_AFNetworking/MRProgressView+AFNetworking.h>
+#endif
+
+#if __has_include(<MRProgress_AFNetworking/MRProgressOverlayView+AFNetworking.h>)
+#import <MRProgress_AFNetworking/MRProgressOverlayView+AFNetworking.h>
+#endif
